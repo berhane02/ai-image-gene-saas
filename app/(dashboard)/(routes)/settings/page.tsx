e256e973642288f7d94b6ea676976ebc6e52db14
berhane02 <yberhane1@outlook.com>
2023-11-30 00:23:29 -0500

    finishup subscription button fuctionality

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "@/components/heading";
+import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription";
 import { Settings } from "lucide-react";
 
@@ -19,6 +20,7 @@ const SettingPage =  async () => {
             ? "You are currently on a pro plan."
             : "You are currently on a free plan."}
         </div>
+        <SubscriptionButton isPro={isPro} />
       </div>
     </div>
   );
--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -1,19 +1,34 @@
+"use client"
+
 import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { Zap } from "lucide-react";
+import { useState } from "react";
 
 interface SubscriptionButtonProps {
-    isPro: boolean;
-};
+  isPro: boolean;
+}
 
-export const SubscriptionButton = (
-    {
-        isPro = false
-    }: SubscriptionButtonProps = ({
-        const onClick = async () => {
-            try {
-                const response = await axios.get("/api/stripe");
-            } catch (error) {
-                console.log("BILLING_ERROR", Error)
-            }
-        }
-    })
-)
+export const SubscriptionButton = ({
+  isPro = false,
+}: SubscriptionButtonProps) => {
+    const [loading, setLoading] = useState(false);
+  const onClick = async () => {
+    try {
+        setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log("BILLING_ERROR", Error);
+    } finally {
+        setLoading(false);
+    }
+  };
+  return (
+    <Button disabled={loading} variant={isPro ? "default" : "premium"} onClick={onClick}>
+      {isPro ? "Manage Subscription" : "Upgrade"}
+      {!isPro && <Zap className="w-4 h-4 ml-2 fill-white" />}
+    </Button>
+  )
+}
