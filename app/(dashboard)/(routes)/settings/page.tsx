8ba3064ebbe9820b77ef4ad1ae96cd20a2b0e593
berhane02 <yberhane1@outlook.com>
2023-12-19 00:37:24 -0500

    delete Heading.tsx

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@/components/heading";
+import {Heading} from "@/components/heading";
 import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription";
 import { Settings } from "lucide-react";
